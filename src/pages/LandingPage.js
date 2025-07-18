55dd877833371de47ac527bb35bda12dcf4e83d2
dharshini100-27 <pmdharshini27@gmail.com>
2025-07-18 13:16:24 +0530

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './LandingPage.css';
+import background from '../assets/landing.jpg';
 
 const LandingPage = () => {
   return (
-    <div className="landing">
-      <div className="overlay">
-        <h1>Welcome to Paradise Nursery</h1>
-        <p>We bring nature into your home with the best collection of houseplants.</p>
-        <Link to="/products" className="get-started-button">Get Started</Link>
-      </div>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        height: '90vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        textShadow: '1px 1px 4px black',
+      }}
+    >
+      <h1>Welcome to Paradise Nursery</h1>
+      <p>Explore our beautiful collection of houseplants to bring your home to life.</p>
+      <Link to="/products">
+        <button style={{ padding: '0.5rem 1rem', fontSize: '1rem' }}>Get Started</button>
+      </Link>
     </div>
   );
 };
