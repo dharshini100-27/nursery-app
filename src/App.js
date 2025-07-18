a472d012c9f502af022d21c9ef57702ad976344e
dharshini100-27 <pmdharshini27@gmail.com>
2025-07-18 13:14:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import ProductPage from './pages/ProductPage';
+import ProductListPage from './pages/ProductListPage';
 import CartPage from './pages/CartPage';
 import Header from './components/Header';
-import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/products" element={<ProductPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <Router basename="/nursery-app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </Router>
   );
 }
 
