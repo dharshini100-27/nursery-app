82df56fa84986a585b4dea34bb9c1c40591e036c
dharshini100-27 <pmdharshini27@gmail.com>
2025-07-18 13:12:13 +0530

    Create plant.js

---
+++ src/data/plant.js
@@ -0,0 +1,46 @@
+const plants = [
+  {
+    id: 1,
+    name: 'Aloe Vera',
+    price: 12.99,
+    category: 'Succulents',
+    image: require('../assets/aloe.jpg')
+  },
+  {
+    id: 2,
+    name: 'Snake Plant',
+    price: 15.49,
+    category: 'Succulents',
+    image: require('../assets/snake.jpg')
+  },
+  {
+    id: 3,
+    name: 'Peace Lily',
+    price: 18.99,
+    category: 'Flowering',
+    image: require('../assets/peace.jpg')
+  },
+  {
+    id: 4,
+    name: 'Monstera',
+    price: 25.99,
+    category: 'Tropical',
+    image: require('../assets/monstera.jpg')
+  },
+  {
+    id: 5,
+    name: 'Pothos',
+    price: 10.99,
+    category: 'Vines',
+    image: require('../assets/pothos.jpg')
+  },
+  {
+    id: 6,
+    name: 'Fiddle Leaf Fig',
+    price: 30.99,
+    category: 'Tropical',
+    image: require('../assets/fiddle.jpg')
+  }
+];
+
+export default plants;
